98267c25fbfcea2c8f45cbb8c27fd30f4bfd0b27
korenAkt <107788923+korenAkt@users.noreply.github.com>
2023-09-22 13:09:39 +0300

    Add files via upload

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,97 @@
+module.exports = {
+  extends: [
+    'eslint:recommended', 
+    'plugin:import/recommended',
+    'airbnb',
+    'import',
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    "jest": true,
+    skipWhitespace: false,
+  },
+  plugins: [
+    'react-hooks'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  parser: 'babel-eslint',
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/extensions': 0,
+    semi: 2,
+
+
+    'ignoreComments': true,
+    'skipComments': false,
+    'react/prop-types': 0,
+    'linebreak-style': 0,
+    'react/state-in-constructor': 0,
+    'import/prefer-default-export': 0,
+    'max-len': [
+      2,
+      500,
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 1,
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_d',
+          '_dh',
+          '_h',
+          '_id',
+          '_m',
+          '_n',
+          '_t',
+          '_text',
+        ],
+      },
+    ],
+    'object-curly-newline': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/alt-text': 0,
+    'jsx-a11y/no-autofocus': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'react/no-array-index-key': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: [
+          'Link',
+        ],
+        specialLink: [
+          'to',
+          'hrefLeft',
+          'hrefRight',
+        ],
+        aspects: [
+          'noHref',
+          'invalidHref',
+          'preferButton',
+        ],
+      },
+    ],
+  },
+};
